c5da9c51ba926eabbde211a4673c552555b96ee9
hyunul <jungwj1023@naver.com>
2024-08-04 16:55:16 +0900

    build: 첫 커밋

---
+++ watering_system/src/main/java/outsourcing/watering_system/WateringSystemApplication.java
@@ -0,0 +1,13 @@
+package outsourcing.watering_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WateringSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WateringSystemApplication.class, args);
+    }
+
+}
---
+++ watering_system/src/test/java/outsourcing/watering_system/WateringSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package outsourcing.watering_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WateringSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
